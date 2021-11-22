5d4676d8e792dca6d8853adfca9a35dc55943d41
wiessall <tristan.wiessalla@embl.it>
2021-11-22 16:22:30 +0100

    implemented the Deck class;

--- deck_of_cards/_nbdev.py
+++ deck_of_cards/_nbdev.py
@@ -2,9 +2,11 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {"Card": "00_card.ipynb"}
+index = {"Card": "00_card.ipynb",
+         "Deck": "01_deck.ipynb"}
 
-modules = ["card.py"]
+modules = ["card.py",
+           "deck.py"]
 
 doc_url = "https://wiessall.github.io/deck_of_cards/"
 
